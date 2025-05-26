412d458d0f42b8774918b68380ba26f4cd57916a
weareandrei <weareandrei@gmail.com>
2025-05-26 19:29:28 +0300

    Added test 'create-podcast' POST endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Hello World!'
   }
+
+  createPodcast(data: { title: string; description?: string }): boolean {
+    console.log('Saving podcast:', data)
+    // Save to DB, queue jobs, etc.
+    return true
+  }
 }
