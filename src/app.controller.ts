6a2e41ab0930a76ebaf6bc55d6d8505bb267e17f
weareandrei <weareandrei@gmail.com>
2025-11-17 03:15:03 +0200

    Removed fake Get method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 import { type PodcastConfig } from './common/types/podcastConfig'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
 
   @Post('create-podcast')
   createPodcast(@Body() payload: PodcastConfig): Promise<void> {
