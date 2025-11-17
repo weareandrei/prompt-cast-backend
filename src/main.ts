461e2b4e9aa89382153d9a55ef44d2c9ee7954ca
weareandrei <weareandrei@gmail.com>
2025-11-17 03:37:04 +0200

    Implemented Nest.js style of handling env variables

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-import dotenv from 'dotenv'
-dotenv.config()
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT ?? 3000)
